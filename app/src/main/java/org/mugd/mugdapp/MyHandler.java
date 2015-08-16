871db132c0e39a98cc1f7538f301e322c47e9ac0
adishjain <adish.jain1995@ymail.com>
2015-08-16 14:08:15 +0530

    push notification outline calsses added

---
+++ app/src/main/java/org/mugd/mugdapp/MyHandler.java
@@ -0,0 +1,67 @@
+package org.mugd.mugdapp;
+
+import com.microsoft.windowsazure.notifications.NotificationsHandler;import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+
+/**
+ * Created by Adish Jain on 16-08-2015.
+ */
+public class MyHandler extends NotificationsHandler {
+    public static final int NOTIFICATION_ID = 1;
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+    Context ctx;
+
+    @Override
+    public void onRegistered(Context context, final String gcmRegistrationId) {
+        super.onRegistered(context, gcmRegistrationId);
+        new AsyncTask<Void, Void, Void>() {
+
+            protected Void doInBackground(Void... params) {
+                try {
+                    notifyme.mClient.getPush().register(gcmRegistrationId, null);
+                    return null;
+                }
+                catch(Exception e) {
+                    // handle error
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    @Override
+    public void onReceive(Context context, Bundle bundle) {
+        ctx = context;
+        String nhMessage = bundle.getString("TESTING PUSH(notification)");
+
+        sendNotification(nhMessage);
+    }
+
+    private void sendNotification(String msg) {
+        mNotificationManager = (NotificationManager)
+                ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(ctx, 0,
+                new Intent(ctx, notifyme.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(ctx)
+                        .setSmallIcon(R.drawable.fab_background)
+                        .setContentTitle("Notification Demo")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+
+    }
+}
+

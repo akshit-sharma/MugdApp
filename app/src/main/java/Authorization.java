daf648cdc133a6d8f405d5ed3cdc25e46d4f826a
akshit-sharma <1ashkit@gmail.com>
2015-08-23 00:59:58 +0530

    Added authorization file

---
+++ app/src/main/java/Authorization.java
@@ -0,0 +1,9 @@
+/**
+ * Created by akshit on 23-08-2015.
+ */
+public class Authorization {
+
+    private static final String url = "<Enter url here>";
+    private static final String key = "<Enter key here>";
+
+}
